ad3d0c95dada8fc62f30cbd05bf6776f22130ed7
LoelDev <pmpinzasr@hotmail.com>
2021-10-21 22:22:15 -0500

    feature: Añadido el dialog de crear equipo y añadir miembros de equipo

---
+++ src/app/components/dialogs/create-team-dialog/create-team-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-team-dialog',
+  templateUrl: './create-team-dialog.component.html',
+  styleUrls: ['./create-team-dialog.component.css']
+})
+export class CreateTeamDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
