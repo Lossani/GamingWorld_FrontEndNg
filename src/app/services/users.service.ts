c96318def8bc656c8208680de980eb6658b6ef9e
thecatuniverse <aaron.plan@hotmail.com>
2021-11-15 17:03:27 -0500

    refactor: Missing files.

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import {Observable, throwError} from "rxjs";
+import {Tournament} from "../entities/tournament-entity";
+import {catchError, map, retry} from "rxjs/operators";
+import {HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpResponse} from "@angular/common/http";
+import {User} from "../entities/user-entity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  private baseURL = "http://localhost:8080";
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+    observe: 'response' as 'body'
+  }
+
+  constructor(private http: HttpClient) { }
+
+  // API Error Handling
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // Default error handling
+      console.log(`An error occurred: ${error.error.message} `);
+    } else {
+      // Unsuccessful Response Error Code returned from Backend
+      console.error(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
+    }
+    // Return Observable with Error Message to Client
+    return throwError('Something happened with request, please try again later');
+  }
+
+  postLogin(item: any):Observable<any> {
+    return this.http.post<any>(`${this.baseURL}/login`, JSON.stringify(item), this.httpOptions);
+  }
+
+  getUserByEmail(email: string, token: string){
+
+    let params = new URLSearchParams();
+    params.append('email', email);
+
+    let tempHttpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }),
+      search: params
+    }
+    return this.http.get<User>(`${this.baseURL}/api/v1/users`, tempHttpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+
+
+
+}
