c3c8a7cb4c591ea94f969917fde99f6f2912c20b
LoelDev <pmpinzasr@hotmail.com>
2021-11-21 01:20:57 -0500

    feature: add Internationalization in all tournamnets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'GamingWorld-FrontEnd';
+
+  constructor(public translate: TranslateService) {
+    translate.addLangs(['en', 'es']);
+    translate.setDefaultLang('en');
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang?.match(/en_US|es_ES/) ? browserLang : 'en')
+  }
 }
